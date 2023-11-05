51db87ddd7a66fe1f5f4c0c1d393c2f44c18699a
Tanya Barsainya <112246189+Tanyabarsainya@users.noreply.github.com>
2023-11-05 23:21:59 +0530

    Add files via upload

---
+++ Day5/Operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int x = 3, y = 6;
+
+  cout << (x & y) << endl;
+
+  cout << (x | y) << endl;
+
+  cout << (x << 2) << endl;
+
+  cout << (x << 1) << endl;
+
+   cout<<(1 >> y) <<endl;
+	
+   cout << (2 >> y) << endl;
+
+
+  return 0;
+}
