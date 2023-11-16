40d15fa6b87460277ab8b90687f30d2471f6daba
Tanya Barsainya <112246189+Tanyabarsainya@users.noreply.github.com>
2023-11-16 21:03:26 +0530

    Add files via upload

---
+++ Day_16/p.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+ int gcd(int a, int b)
+{
+	if(b==0)
+		return a;
+
+	return gcd(b, a % b);
+}
+
+int main() {
+    
+    	int a = 10, b = 35;
+    	
+    	cout<<gcd(a, b);
+    	
+    	return 0;
+}
