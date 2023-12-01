1b27d400e6c42fa5196708c27ab34350ec8058c5
Tanya Barsainya <112246189+Tanyabarsainya@users.noreply.github.com>
2023-12-01 18:13:38 +0530

    Add files via upload

---
+++ Day_31/p.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int getSum(int n)
+{
+	if(n == 0)
+		return 0;
+
+	return n + getSum(n - 1);
+
+}
+
+int main() {
+	
+	int n = 9;
+	
+	cout<<getSum(n);
+	
+	return 0;
+}
